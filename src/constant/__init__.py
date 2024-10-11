904f863afe5510200b88bea24f02707ea60f39b5
Arava Choudhary <aravachoudhary11@gmail.com>
2024-10-11 17:01:40 +0530

    AWS Deployment

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -1,13 +1,19 @@
 import os
 
+MODEL_CONGIF_FILE = "config/model.yaml"
+
 AWS_S3_BUCKET_NAME = "waferr-fault"
 MONGO_DATABASE_NAME = "sensordb"
 MONGO_COLLECTON_NAME = "waferfault"
+S3_MODEL_NAME = "model.pkl"
 
 TARGET_COLUMN = "quality"
 MONGO_DB_URL = "mongodb+srv://Aranika:Nainika1234$@cluster0.d2qw0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-MODEL_FILE_NAME = "model"
+MODEL_FILE_NAME = "model.pkl"
 MODEL_FILE_EXTENSION = ".pkl"
 
 artifact_folder = 'artifacts'
+
+APP_HOST = "0.0.0.0"
+APP_PORT = 8080
